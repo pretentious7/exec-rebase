3a5c8f3413b8b5891e6dd5b30cec4b3ace545b36
Abhishek Cherath <abhicherath@gmail.com>
2022-07-05 00:28:02 -0400

    run loop for each commit

--- exec-rebase.sh
+++ exec-rebase.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
 
 # So, the way to do this is
 # checkout formatting file from branch
@@ -9,3 +11,20 @@
 # repeat until you get to root of branch
 # then cherry-pick until queue empty
 # now rebase onto target
+
+PARENT_COMMIT_REF=${1:-}
+if [[ -z "$PARENT_COMMIT_REF" ]]; then
+  echo "Parent commit ref must be defined"
+  exit 1
+fi
+
+PARENT_COMMIT_HASH=$(git rev-parse "$PARENT_COMMIT_REF")
+#TODO: write trap to handle if invalid ref
+
+
+while [[ $(git rev-parse HEAD) != "$PARENT_COMMIT_HASH" ]]; do
+  echo "yay"
+done  
+#git checkout HEAD^
+
+#echo "$PARENT_COMMIT_REF"
